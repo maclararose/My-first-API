c2ec56c91f4532091db0bfe593a448c61e70142d
Maria Clara <mcbarroso.contato@gmail.com>
2021-11-19 00:42:58 -0300

    Add front for consuming API

---
+++ api_task_user/application.js
@@ -0,0 +1,18 @@
+function getTasks() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      let container = document.getElementById("container");
+      let responses = JSON.parse(this.responseText);
+
+      for(var i = 0; i < responses.length ; i++) {
+        container.insertAdjacentHTML("beforeend", `[${responses[i].id}]: ${responses[i].task} <br/>`);
+      };
+    };
+  };
+
+  xhttp.open("GET", "http://localhost:3001/todos", true);
+  xhttp.send();
+};
+
+getTasks();
